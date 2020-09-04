3ab6002c52b258d41a4a4db74c1548e530589327
Ajay Bava <ajaybava1998@gmail.com>
2020-09-04 13:58:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Pub
+
+Personal portfolio built using the good old basics. Made with Vanilla JS, CSS and HTML.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Installing
+
+1. Clone the repo
+
+```
+git clone https://github.com/Ajazz1998/Pub.git
+```
+
+2. Open home file
+
+```
+open index.html
+```
+
+4. Explore
+
+```
+Enjoy :)
+```
+
+## Built With
+
+* HTML5
+* CSS
+* JavaScript
+
+
---
+++ script.js
@@ -0,0 +1,102 @@
+$(document).ready(function(){
+    $(".category_item").click(function(){
+        var catagory = $(this).attr("id");
+        
+        if(catagory == "all"){
+            $(".items").hide();
+            setTimeout(function(){
+                $(".items").show();
+            }, 300)
+        } else {
+            $(".items").hide();
+            setTimeout(function(){
+                $("." + catagory).show();
+            }, 300)
+        }
+    });
+
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+          event.preventDefault();
+    
+          var hash = this.hash;
+    
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 1000, function(){
+       
+            window.location.hash = hash;
+          });
+        } 
+      });
+
+       /* Animate scroll */
+        
+         $('.js--wp-1').waypoint(function(){
+             $('.js--wp-1').addClass('animated fadeIn'); 
+         }, {
+             offset: '50%'
+         });
+      
+         $('.js--wp-2').waypoint(function(){
+          $('.js--wp-2').addClass('animated fadeInUp'); 
+         }, {
+             offset: '100%'
+         });
+      
+         $('.js--wp-footer').waypoint(function(){
+          $('.js--wp-footer').addClass('animated fadeInUp'); 
+         }, {
+             offset: '80%'
+         });
+
+         $('.js--wp-7').waypoint(function(){
+          $('.js--wp-7').addClass('animated fadeInUp'); 
+         }, {
+             offset: '70%'
+         });
+
+         $('.js--wp-5').waypoint(function(){
+          $('.js--wp-5').addClass('animated fadeInUp'); 
+         }, {
+             offset: '80%'
+         });
+
+         $('.js--wp-6').waypoint(function(){
+          $('.js--wp-6').addClass('animated fadeInUp'); 
+         }, {
+             offset: '100%'
+         });
+         
+         $('.js--wp-3').waypoint(function(){
+             $('.js--wp-3').addClass('animated fadeIn'); 
+         }, {
+             offset: '50%'
+         });
+         
+         $('.js--wp-4').waypoint(function(){
+             $('.js--wp-4').addClass('animated pulse'); 
+         }, {
+             offset: '50%'
+         });
+
+         $('.js--wp-8').waypoint(function(){
+             $('.js--wp-8').addClass('animated pulse'); 
+         }, {
+             offset: '50%'
+         });
+});
+
+document.getElementById("navicon").addEventListener("click", () => {
+
+    const topbar = document.getElementById("topbar");
+    
+    if(topbar.style.display === "none"){
+        topbar.style.display="block";
+    }else{
+        topbar.style.display="none";
+    }
+})
+
+
